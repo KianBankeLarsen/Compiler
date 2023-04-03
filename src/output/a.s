192b5a07afe56da8928f1638d760f9ccff68744b
Kian Banke Larsen <snubikian@gmail.com>
2023-04-03 22:33:16 +0200

    Checking that WSL works

--- src/output/a.s
+++ src/output/a.s
@@ -18,17 +18,51 @@ main:
                 pushq %rbp
                 movq %rsp, %rbp
 
+                subq $0, %rsp
+                pushq $1
+
+                pushq %rcx
+                pushq %rdx
+                pushq %rsi
+                pushq %rdi
+                pushq %r8
+                pushq %r9
+                pushq %r10
+                pushq %r11
+
+                movq %rbp, %rdx
+                pushq %rdx
                 subq $8, %rsp
-                pushq $4
+
+                pushq %rbx
+                pushq %r12
+                pushq %r13
+                pushq %r14
+                pushq %r15
+
+                pushq %rbp
+                movq %rsp, %rbp
+
+                subq $0, %rsp
+for_0:
                 movq %rbp, %rdx
-                popq -8(%rdx)
-                movq %rbp, %rdx
-                pushq -8(%rdx)
+                pushq 128(%rdx)
                 pushq $5
                 popq %rbx
                 popq %rcx
-                imulq %rbx, %rcx
-                pushq %rcx
+                cmpq %rbx, %rcx
+                jl cmp_true_2
+                pushq $0
+                jmp cmp_end_3
+cmp_true_2:
+                pushq $1
+cmp_end_3:
+                popq %rbx
+                movq $0, %rcx
+                cmpq %rbx, %rcx
+                je rof_1
+                movq %rbp, %rdx
+                pushq 128(%rdx)
 
                 pushq %rcx
                 pushq %rdx
@@ -46,18 +80,18 @@ main:
                 andq $-16, %rsp
                 movq $0, %rbx
                 cmpq %rsp, %rcx
-                je aligned_0
+                je aligned_4
                 incq %rbx
-aligned_0:
+aligned_4:
                 pushq %rbx
                 subq $8, %rsp
                 callq printf@plt
                 addq $8, %rsp
                 popq %rbx
                 cmpq $0, %rbx
-                je aligned_1
+                je aligned_5
                 addq $8, %rsp
-aligned_1:
+aligned_5:
                 addq $8, %rsp
 
 
@@ -70,8 +104,42 @@ aligned_1:
                 popq %rdx
                 popq %rcx
 
-end_main:
+                movq %rbp, %rdx
+                pushq 128(%rdx)
+                pushq $1
+                popq %rbx
+                popq %rcx
+                addq %rbx, %rcx
+                pushq %rcx
+                movq %rbp, %rdx
+                popq 128(%rdx)
+                jmp for_0
+rof_1:
+                addq $0, %rsp
+                movq %rbp, %rsp
+                popq %rbp
+
+                popq %r15
+                popq %r14
+                popq %r13
+                popq %r12
+                popq %rbx
+
+
                 addq $8, %rsp
+
+                popq %r11
+                popq %r10
+                popq %r9
+                popq %r8
+                popq %rdi
+                popq %rsi
+                popq %rdx
+                popq %rcx
+
+                addq $8, %rsp
+end_main:
+                addq $0, %rsp
                 movq %rbp, %rsp
                 popq %rbp
 
