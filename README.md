0598d37833dd8346c1621ecb57833b129bb2193f
Kian Banke Larsen <snubikian@gmail.com>
2023-04-11 18:17:04 +0200

    Started writing README

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-# Compiler
+# Using Panda Compiler
+
+A ```main.py``` file has been created in the outer folder that makes it possible to easily interact with the compiler located in ```src/```. 
+
+The main file handles the instantiation and therefore also the running of the ```PandaCompiler``` class. ```Argparse``` is used to take command line arguments. 
+
+Running the follow command,
+
+```
+$ python3.10 main.py --help
+
+usage: Compiler for Panda [-h] [-o OUTPUT] [-c] [-d] [-f FILE] [-t] [-r]
+
+Compiles source code to assembly
+
+options:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        Specify name of assembly output file
+  -c, --compile         Set this flag if the output file should be compilled with gcc
+  -d, --debug           Set this flag for debugging information, i.e., ILOC and Graphviz
+  -f FILE, --file FILE  Path to input file, otherwise stdin will be used
+  -t, --runTests        Run tests
+  -r, --run             Run compilled program
+```
+
+makes it possible to see what options are available.
+
+# The compiler
 
 ## AST
-![](./src/printer/images/AST.a.gv.png)
+![](src/printer/images/AST.src/output/a.gv.png)
 
+## AST de-sugared
+![](src/printer/images/AST-desugar.src/output/a.gv.png)
 
 ## Symbol Table
-![](./src/printer/images/Symbol.a.gv.png)
+![](src/printer/images/Symbol.src/output/a.gv.png)
+
+
+# Testing
--- src/output/a.s
+++ src/output/a.s
@@ -18,16 +18,125 @@ main:
                 pushq %rbp
                 movq %rsp, %rbp
 
-                subq $24, %rsp
+                subq $0, %rsp
+                pushq $1
+
+                pushq %rcx
+                pushq %rdx
+                pushq %rsi
+                pushq %rdi
+                pushq %r8
+                pushq %r9
+                pushq %r10
+                pushq %r11
+
+                movq %rbp, %rdx
+                pushq %rdx
+                subq $8, %rsp
+
+                pushq %rbx
+                pushq %r12
+                pushq %r13
+                pushq %r14
+                pushq %r15
+
+                pushq %rbp
+                movq %rsp, %rbp
+
+                subq $0, %rsp
+for_0:
+                movq %rbp, %rdx
+                pushq 128(%rdx)
                 pushq $5
+                popq %rbx
+                popq %rcx
+                cmpq %rbx, %rcx
+                jl cmp_true_2
+                pushq $0
+                jmp cmp_end_3
+cmp_true_2:
+                pushq $1
+cmp_end_3:
+                popq %rbx
+                movq $0, %rcx
+                cmpq %rbx, %rcx
+                je rof_1
                 movq %rbp, %rdx
-                popq -8(%rdx)
-                pushq $10
+                pushq 128(%rdx)
+
+                pushq %rcx
+                pushq %rdx
+                pushq %rsi
+                pushq %rdi
+                pushq %r8
+                pushq %r9
+                pushq %r10
+                pushq %r11
+
+                leaq form(%rip), %rdi
+                movq 64(%rsp), %rsi
+                movq $0, %rax
+                movq %rsp, %rcx
+                andq $-16, %rsp
+                movq $0, %rbx
+                cmpq %rsp, %rcx
+                je aligned_4
+                incq %rbx
+aligned_4:
+                pushq %rbx
+                subq $8, %rsp
+                callq printf@plt
+                addq $8, %rsp
+                popq %rbx
+                cmpq $0, %rbx
+                je aligned_5
+                addq $8, %rsp
+aligned_5:
+                addq $8, %rsp
+
+
+                popq %r11
+                popq %r10
+                popq %r9
+                popq %r8
+                popq %rdi
+                popq %rsi
+                popq %rdx
+                popq %rcx
+
                 movq %rbp, %rdx
-                popq -16(%rdx)
-                pushq $34534
+                pushq 128(%rdx)
+                pushq $1
+                popq %rbx
+                popq %rcx
+                addq %rbx, %rcx
+                pushq %rcx
                 movq %rbp, %rdx
-                popq -24(%rdx)
+                popq 128(%rdx)
+                jmp for_0
+rof_1:
+                movq %rbp, %rsp
+                popq %rbp
+
+                popq %r15
+                popq %r14
+                popq %r13
+                popq %r12
+                popq %rbx
+
+
+                addq $8, %rsp
+
+                popq %r11
+                popq %r10
+                popq %r9
+                popq %r8
+                popq %rdi
+                popq %rsi
+                popq %rdx
+                popq %rcx
+
+                addq $8, %rsp
 end_main:
                 movq %rbp, %rsp
                 popq %rbp
